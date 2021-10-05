8c7e01e0521ac0a2331b658a3964fe5e93e62907
Faridz Chandra <faridzkurniaa@gmail.com>
2021-10-05 14:23:46 +0700

    Sign Up Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:viper/pages/sign_in_page.dart';
+import 'package:viper/pages/sign_up_page.dart';
 import 'package:viper/pages/splash_page.dart';
 
 void main() => runApp(MyApp());
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       routes: {
         '/': (context) => SplashPage(),
-        '/sign-in': (context) => SignInPage()
+        '/sign-in': (context) => SignInPage(),
+        '/sign-up': (context) => SignUpPage(),
       },
     );
   }
