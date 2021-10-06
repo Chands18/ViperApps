0354f27a7687e19e828e759ab36c0653c4cb7746
Faridz Chandra <faridzkurniaa@gmail.com>
2021-10-06 12:17:35 +0700

    chat page

--- lib/pages/home/chat_page.dart
+++ lib/pages/home/chat_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:viper/theme.dart';
+import 'package:viper/widgets/chat_tile.dart';
 
 class ChatPage extends StatelessWidget {
   @override
@@ -20,7 +21,7 @@ class ChatPage extends StatelessWidget {
       );
     }
 
-    Widget content() {
+    Widget emptyChat() {
       return Expanded(
         child: Container(
           width: double.infinity,
@@ -76,6 +77,23 @@ class ChatPage extends StatelessWidget {
                   ),
                 ),
               ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    Widget content() {
+      return Expanded(
+        child: Container(
+          width: double.infinity,
+          color: backgroundColor3,
+          child: ListView(
+            padding: EdgeInsets.symmetric(
+              horizontal: defaultMargin,
+            ),
+            children: [
+              ChatTile(),
             ],
           ),
         ),
