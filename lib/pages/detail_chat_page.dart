85904b68c79dca9ebe6c030cacf85d3d09b1310b
Faridz Chandra <faridzkurniaa@gmail.com>
2021-10-14 22:52:53 +0700

    detail chat

--- lib/pages/detail_chat_page.dart
+++ lib/pages/detail_chat_page.dart
@@ -157,6 +157,7 @@ class DetailChatPage extends StatelessWidget {
           ChatBubble(
             isSender: true,
             text: 'Hi,This item is still available?',
+            hasProduct: true,
           ),
           ChatBubble(
             isSender: false,
