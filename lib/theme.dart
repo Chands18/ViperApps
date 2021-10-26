424794f527ff97d8db3e5f0e8613b020985f9336
Faridz Chandra <faridzkurniaa@gmail.com>
2021-10-26 12:06:01 +0700

    add backgroundcolor6

--- lib/theme.dart
+++ lib/theme.dart
@@ -12,6 +12,7 @@ Color backgroundColor2 = Color(0xff2B2937);
 Color backgroundColor3 = Color(0xff242231);
 Color backgroundColor4 = Color(0xff252836);
 Color backgroundColor5 = Color(0xff2B2844);
+Color backgroundColor6 = Color(0xffECEDEF);
 Color primaryTextColor = Color(0xffE1E1E1);
 Color secondaryTextColor = Color(0xff999999);
 Color subtitleColor = Color(0xff504F5E);
