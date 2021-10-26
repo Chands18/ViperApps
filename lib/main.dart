1f99c94b16aa763fc1fdc360075cd9c8a886634c
Faridz Chandra <faridzkurniaa@gmail.com>
2021-10-26 11:09:08 +0700

    profile page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:viper/pages/detail_chat_page.dart';
+import 'package:viper/pages/edit_profile_page.dart';
 import 'package:viper/pages/home/main_page.dart';
 import 'package:viper/pages/sign_in_page.dart';
 import 'package:viper/pages/sign_up_page.dart';
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
         '/sign-up': (context) => SignUpPage(),
         '/home': (context) => MainPage(),
         '/detail-chat': (context) => DetailChatPage(),
+        '/edit-profile': (context) => EditProfilePage(),
       },
     );
   }
--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -45,9 +45,15 @@ class ProfilePage extends StatelessWidget {
                     ],
                   ),
                 ),
-                Image.asset(
-                  'assets/button_exit.png',
-                  width: 20,
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamedAndRemoveUntil(
+                        context, '/sign-in', (route) => false);
+                  },
+                  child: Image.asset(
+                    'assets/button_exit.png',
+                    width: 20,
+                  ),
                 )
               ],
             ),
@@ -56,9 +62,93 @@ class ProfilePage extends StatelessWidget {
       );
     }
 
+    Widget menuItem(String text) {
+      return Container(
+        margin: EdgeInsets.only(
+          top: 16,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              text,
+              style: secondaryTextStyle.copyWith(
+                fontSize: 13,
+              ),
+            ),
+            Icon(
+              Icons.chevron_right,
+              color: primaryTextColor,
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget content() {
+      return Expanded(
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+          decoration: BoxDecoration(
+            color: backgroundColor3,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Account',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: semiBold,
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, '/edit-profile');
+                },
+                child: menuItem(
+                  'Edit Profile',
+                ),
+              ),
+              menuItem(
+                'Your Orders',
+              ),
+              menuItem(
+                'Help',
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                'General',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: semiBold,
+                ),
+              ),
+              menuItem(
+                'Privacy Policy',
+              ),
+              menuItem(
+                'terms of Service',
+              ),
+              menuItem(
+                'Rate App',
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
     return Column(
       children: [
         header(),
+        content(),
       ],
     );
   }
