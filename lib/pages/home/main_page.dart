972993481f5f8f7464ac5d8e7429642cf3277a3e
Faridz Chandra <faridzkurniaa@gmail.com>
2021-10-26 12:05:44 +0700

    edit backgorund color

--- lib/pages/home/main_page.dart
+++ lib/pages/home/main_page.dart
@@ -115,7 +115,7 @@ class _MainPageState extends State<MainPage> {
     }
 
     return Scaffold(
-      backgroundColor: backgroundColor1,
+      backgroundColor: currentIndex == 0 ? backgroundColor1 : backgroundColor3,
       floatingActionButton: cartButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: customButtonNav(),
