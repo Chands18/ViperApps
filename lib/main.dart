1e34c14810cd23fe1ec7af7e4dd0cbbe271e4657
faridz <faridzkurniaa@gmail.com>
2022-01-22 15:00:48 +0700

    route checkout page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:viper/pages/cart_page.dart';
 import 'package:viper/pages/checkout_page.dart';
+import 'package:viper/pages/checkout_success_page.dart';
 import 'package:viper/pages/detail_chat_page.dart';
 import 'package:viper/pages/edit_profile_page.dart';
 import 'package:viper/pages/home/main_page.dart';
@@ -26,6 +27,7 @@ class MyApp extends StatelessWidget {
         '/product': (context) => ProductPage(),
         '/cart': (context) => CartPage(),
         '/checkout': (context) => CheckoutPage(),
+        '/checkout-success': (context) => CheckoutSuccessPage(),
       },
     );
   }
