3d80a61c6766727539ba98a829320a4a2cb98264
faridz <faridzkurniaa@gmail.com>
2022-01-22 10:24:43 +0700

    checkout card

---
+++ lib/pages/checkout_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:viper/theme.dart';
+import 'package:viper/widgets/checkout_card.dart';
+
+class CheckoutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          'Checout Details',
+        ),
+      );
+    }
+
+    Widget content() {
+      return ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: defaultMargin,
+        ),
+        children: [
+          // Note: List Items
+          Container(
+            margin: EdgeInsets.only(
+              top: defaultMargin,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'List Items',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                ),
+                CheckoutCard(),
+              ],
+            ),
+          ),
+        ],
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor3,
+      appBar: header(),
+      body: content(),
+    );
+  }
+}
